db2e7daefbefe4447dceaf79c6658218a1a2dc65
XiaoWen Su <2014863935@qq.com>
2024-12-18 17:36:45 +0800

    微信授权成功和token鉴权成功分离开来，登陆成功后逻辑抽离。

--- peanut-chat-app-server/src/main/java/com/sici/chat/ws/WsNettyImServerStarter.java
+++ peanut-chat-app-server/src/main/java/com/sici/chat/ws/WsNettyImServerStarter.java
@@ -7,6 +7,10 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
@@ -45,6 +49,22 @@ public class WsNettyImServerStarter implements InitializingBean {
                 log.info("channel initialized, channel:{}", channel);
                 // 设置消息处理器handler
                 channel.pipeline().addLast(new IdleStateHandler(30, 0, 0));
+
+                // 因为使用http协议，所以需要使用http的编码器，解码器
+                channel.pipeline().addLast(new HttpServerCodec());
+                // 以块方式写，添加 chunkedWriter 处理器
+                channel.pipeline().addLast(new ChunkedWriteHandler());
+                /**
+                 * 说明：
+                 *  1. http数据在传输过程中是分段的，HttpObjectAggregator可以把多个段聚合起来；
+                 *  2. 这就是为什么当浏览器发送大量数据时，就会发出多次 http请求的原因
+                 */
+                channel.pipeline().addLast(new HttpObjectAggregator(8192));
+                //保存用户ip
+//                channel.pipeline().addLast(new HttpHeadersHandler());
+                
+                
+                channel.pipeline().addLast(new WebSocketServerProtocolHandler("/"));
                 channel.pipeline().addLast(applicationContext.getBean(WsServerCoreHandler.class));
             }
         });
--- peanut-chat-app-server/src/main/java/com/sici/chat/ws/channel/ChannelAttr.java
+++ peanut-chat-app-server/src/main/java/com/sici/chat/ws/channel/ChannelAttr.java
@@ -14,5 +14,5 @@ import io.netty.util.AttributeKey;
 public class ChannelAttr {
     public static AttributeKey<Integer> USER_ID = AttributeKey.valueOf("userId");
     public static AttributeKey<Integer> IP = AttributeKey.valueOf("ip");
-    public static AttributeKey<Integer> TOKEN = AttributeKey.valueOf("token");
+    public static AttributeKey<String> TOKEN = AttributeKey.valueOf("token");
 }
